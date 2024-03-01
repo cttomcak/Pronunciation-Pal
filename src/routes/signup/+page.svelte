ead9e4e345bddde24c594d2e3027b48709076e2c
Fraggenard <fraggenard@gmail.com>
2024-02-29 19:39:14 -0600

    barebones signup page and homepage login link

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,24 @@
+<section>
+    <form method="post">
+        <div class="group">
+            <label for="first_name">First Name</label>
+            <input type="first_name" name="first_name" id="first_name" required />
+            <label for="last_name">Last Name</label>
+            <input type="last_name" name="last_name" id="last_name" required />
+        </div>
+
+        <div class="group">
+			<label for="email">Email</label>
+			<input type="email" name="email" id="email" required />
+		</div>
+
+        <div class="group">
+			<label for="password">Password</label>
+			<input type="password" name="password" id="password" required />
+		</div>
+    </form>
+
+    <div class="submit-container">
+        <button type="submit">Sign Up</button>
+    </div>
+</section>
