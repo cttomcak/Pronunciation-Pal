804e74819f795f3f768e5995ecc20ec8559401d7
Matthew Heywood <matthew@bytethebullets.com>
2024-03-21 22:39:54 -0500

    Restyle viseme cards to be more visually appealing

--- src/routes/VisemeImage.svelte
+++ src/routes/VisemeImage.svelte
@@ -9,28 +9,38 @@
     <img src={`visemes/${viseme}.jpg`} class="viseme-image" alt={`Viseme ${viseme}`}>
     <!-- The angled viseme image -->
     <img src={`visemes/${viseme}45.jpg`} class="viseme-image" alt={`Viseme ${viseme} Angled`}>
-    <div>
-        <strong>From phoneme: "{phoneme}"</strong>
-    </div>
-    <div>
-        <strong>Viseme: {viseme}</strong>
-    </div>
-    <div>
-        <strong>Description: {viseme_description_dict[viseme]}</strong>
-    </div>
+    <!-- TODO: Make a toggle for phoneme/viseme -->
+    <h3 class="card-title">{viseme}</h3>
+    <p class="viseme-description">
+        {viseme_description_dict[viseme]}
+    </p>
 </div>
 
 <style>
+    .card-title {
+        text-align: center;
+        margin: 10px 0 0 0;
+    }
+    .viseme-description::before {
+        content: "• ";
+    }
+    .viseme-description {
+        font-size: .8em;
+        margin: 0 0 10px 10px;
+    }
     .viseme-image {
         width: 100%; /* Set width of images */
         height: auto; /* Maintain aspect ratio */
     }
 
     .image_div {
-        width: 20%;
+        width: 15%;
         min-width: 200px;
         margin: 5px; /* Add some space between images */
-        background-color: rgb(160, 193, 230);;
+        background-color: rgb(255, 255, 255);;
+        border-radius: 20px;
+        background: #ffffff;
+        box-shadow:  2px 4px 16px rgba(0, 0, 0, 0.123);
         padding: 10px;
     }
 </style>
